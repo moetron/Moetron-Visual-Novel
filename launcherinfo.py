1573487f16ef51667ec7838b8d978c885f8ca73d
OkDucky <musicalbudgie@gmail.com>
2011-08-30 22:23:52 +0100

    second commit

---
+++ launcherinfo.py
@@ -0,0 +1,8 @@
+documentation_extensions = 'txt html'
+build_mac = False
+build_all = False
+build_windows = False
+distribution_base = 'moetron'
+ignore_extensions = '~ .bak'
+build_linux = True
+executable_name = 'moetron'
